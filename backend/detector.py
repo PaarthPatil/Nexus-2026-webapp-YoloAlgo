307c98a82a87a92a0986ac446c5be692cfed40d7
PaarthPatil <75310995+PaarthPatil@users.noreply.github.com>
2026-03-21 12:14:39 +0530

    Delete backend directory

--- backend/detector.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-def get_count():
-    # temporary test image (we'll switch to webcam next)
-    results = model("https://ultralytics.com/images/bus.jpg")
-    return len(results[0].boxes)
